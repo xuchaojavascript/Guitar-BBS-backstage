573284d3ac96cf457bd934d33d1ae1fdad6ca36b
yichar <1454030573@qq.com>
2019-11-16 21:07:56 +0800

    吉它社区后台代码更新

---
+++ src/views/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="text" v-model="data.data.b">
+    <button @click="viewData">查看数据</button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      data: {
+        data: {
+          b: ''
+        }
+      },
+      date: {}
+    }
+  },
+  methods: {
+    viewData () {
+      console.log(this.data)
+      console.log(this.date)
+    }
+  },
+  mounted () {
+    var a = {
+      b: 12
+    }
+    this.data = {
+      data: a
+    }
+    this.date = {
+      data: a
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
